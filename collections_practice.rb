14a58103d0f41909e74f779616396c26532e987b
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-11-19 10:59:50 -0500

    commit tree 1ab8f61c456c2ab28f7057a914ad4cc1eb722786

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        a<=>b 
+    end
 
+end
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b<=>a
+    end
+end
+
+def sort_array_char_count(array)
+    array = array.sort_by(&:length)
+    
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+    
+end
+
+def reverse_array(array)
+    array = array.reverse
+end
+def kesha_maker(array)
+    array.each do |n|
+        n[2] = "$"
+    end
+end
+def find_a(array)
+    array.find_all do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    i = 0
+    array.each do |n|
+        i += n
+    end
+    i
+end
+
+def add_s(array)
+    array.collect do |word|
+      if array[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
+  end
